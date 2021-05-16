99b8c76f08b19106c4067b82a654bbf28ce8a5fa
Olga Tsuprik <tt.olia@mail.ru>
2021-05-17 00:13:05 +0300

    ADDED Tasks

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger itemsAmount = 0;
+        for (NSNumber *a in array){
+            for (NSNumber *b in array){
+                int result = ([a intValue] - [b intValue]);
+                if ([[NSNumber numberWithInt:result] isEqualToNumber:number]) {
+                    itemsAmount += 1;
+                }
+            }
+        }
+        return itemsAmount;
 }
 
 @end
